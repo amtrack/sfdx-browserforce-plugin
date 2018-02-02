e4350238b0293110f0c8aa982f13f71f83597a9e
Matthias Rolke <mr.amtrack@gmail.com>
2018-02-02 17:03:35 +0100

    fix(test): set timeout

--- test/browserforce.js
+++ test/browserforce.js
@@ -4,12 +4,16 @@ const Browserforce = require("../lib/browserforce");
 describe("Browserforce", () => {
   describe("constructor()", () => {
     it("should instantiate Browserforce", function() {
+      this.timeout(1000 * 15);
+      this.slow(1000 * 5);
       let bf = new Browserforce();
       assert(bf !== undefined);
     });
   });
   describe("launch()", () => {
     it("should return a browser instance", async function() {
+      this.timeout(1000 * 15);
+      this.slow(1000 * 5);
       let bf = new Browserforce();
       let browser = await bf.launch();
       assert(browser !== undefined);
