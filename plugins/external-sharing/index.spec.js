f1005803ee23994009644ad699704cf380c988a4
Matthias Rolke <mr.amtrack@gmail.com>
2018-03-01 10:33:47 +0100

    Revert "docs: update example to disable ExternalSharing"
    This reverts commit e58945256616c648a2ff0f77d964ea372518dc2d.
    
    Salesforce has silently changed the default scratch org to have
    ExternalSharing disabled by default.
    
    According to this [thread](https://success.salesforce.com/0D53A00003SQizw),
    the feature will be supported soon.
    Disabled External Sharing Model will remain the default.

--- plugins/external-sharing/index.spec.js
+++ plugins/external-sharing/index.spec.js
@@ -5,16 +5,9 @@ const path = require("path");
 const pluginName = "ExternalSharing";
 
 describe(pluginName, () => {
-  it("should disable and re-enable", function() {
+  it("should enable and disable", function() {
     this.timeout(1000 * 120);
     this.slow(1000 * 30);
-    var disableCmd = child.spawnSync("sfdx", [
-      "browserforce:shape:apply",
-      "-f",
-      path.resolve(path.join(__dirname, "disable.json"))
-    ]);
-    assert.deepEqual(disableCmd.status, 0, disableCmd.stderr);
-    assert(/to 'false'/.test(disableCmd.stdout.toString()));
     var enableCmd = child.spawnSync("sfdx", [
       "browserforce:shape:apply",
       "-f",
@@ -22,5 +15,12 @@ describe(pluginName, () => {
     ]);
     assert.deepEqual(enableCmd.status, 0, enableCmd.stderr);
     assert(/to 'true'/.test(enableCmd.stdout.toString()));
+    var disableCmd = child.spawnSync("sfdx", [
+      "browserforce:shape:apply",
+      "-f",
+      path.resolve(path.join(__dirname, "disable.json"))
+    ]);
+    assert.deepEqual(disableCmd.status, 0, disableCmd.stderr);
+    assert(/to 'false'/.test(disableCmd.stdout.toString()));
   });
 });
