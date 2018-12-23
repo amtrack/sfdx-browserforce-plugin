900bbf7beca2ba2fa8e66db9cd30bab80d946c5e
Matthias Rolke <mr.amtrack@gmail.com>
2018-12-23 17:44:46 +0100

    refactor: remove unused variables

--- src/plugins/customer-portal/portals/index.ts
+++ src/plugins/customer-portal/portals/index.ts
@@ -5,13 +5,10 @@ import { removeNullValues, semanticallyCleanObject } from '../../utils';
 
 const PATHS = {
   LIST_VIEW: '_ui/core/portal/CustomerSuccessPortalSetup/d',
-  EDIT_VIEW: '_ui/core/portal/CustomerSuccessPortalSetup/e',
   PORTAL_PROFILE_MEMBERSHIP: '_ui/core/portal/PortalProfileMembershipPage/e'
 };
 const SELECTORS = {
-  ENABLED: '#penabled',
   SAVE_BUTTON: 'input[name="save"]',
-  ERROR_DIV: '#errorTitle',
   ERROR_DIVS: 'div.errorMsg',
   LIST_VIEW_PORTAL_LINKS_XPATH:
     '//div[contains(@class,"pbBody")]//th[contains(@class,"dataCell")]//a[starts-with(@href, "/060")]',
@@ -23,8 +20,7 @@ const SELECTORS = {
   PORTAL_SELF_REG_USER_DEFAULT_ROLE_ID: 'SelfRegUserDefaultRole',
   PORTAL_SELF_REG_USER_DEFAULT_PROFILE_ID: 'SelfRegUserDefaultProfile',
   PORTAL_PROFILE_MEMBERSHIP_PROFILES: 'th.dataCell',
-  PORTAL_PROFILE_MEMBERSHIP_CHECKBOXES: 'td.dataCell input',
-  CUSTOM_OBJECT_AVAILABLE_FOR_CUSTOMER_PORTAL: '#options_9'
+  PORTAL_PROFILE_MEMBERSHIP_CHECKBOXES: 'td.dataCell input'
 };
 
 export default class CustomerPortalSetup extends BrowserforcePlugin {
