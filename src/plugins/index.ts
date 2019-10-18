4e3f3b108a65221fd6b67f4381dff884bae387c6
Matthias Rolke <mr.amtrack@gmail.com>
2019-10-18 21:45:24 +0200

    feat: add User Interface -> Density Settings
    Note: This feature is dependent on the language of the user
    and will currently only work with English.

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -1,6 +1,7 @@
 import * as communities from './communities';
 import * as companySettings from './company-settings';
 import * as customerPortal from './customer-portal';
+import * as densitySettings from './density-settings';
 import * as homePageLayouts from './home-page-layouts';
 import * as salesforceToSalesforce from './salesforce-to-salesforce';
 import * as security from './security';
@@ -8,6 +9,7 @@ export {
   communities,
   companySettings,
   customerPortal,
+  densitySettings,
   homePageLayouts,
   salesforceToSalesforce,
   security
