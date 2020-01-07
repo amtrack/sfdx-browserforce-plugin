7b5fa8b067daf68d2b23e03dff969dd63dfa4c21
Matthias Rolke <mr.amtrack@gmail.com>
2020-01-07 14:50:02 +0100

    chore: fix linting errors in test dir

--- test/utils.test.ts
+++ test/utils.test.ts
@@ -2,22 +2,22 @@ import * as assert from 'assert';
 import { semanticallyCleanObject } from '../src/plugins/utils';
 
 describe('semanticallyCleanObject', () => {
-  it('should clean object', async function() {
+  it('should clean object', async () => {
     assert.deepStrictEqual(semanticallyCleanObject({ id: 'a2' }), null);
   });
-  it('should clean object with custom id', async function() {
+  it('should clean object with custom id', async () => {
     assert.deepStrictEqual(
       semanticallyCleanObject({ myid: 'a2' }, 'myid'),
       null
     );
   });
-  it('should return object as is', async function() {
+  it('should return object as is', async () => {
     assert.deepStrictEqual(semanticallyCleanObject({ id: 'a2', a: 'hi' }), {
       id: 'a2',
       a: 'hi'
     });
   });
-  it('should return object as is with custom id', async function() {
+  it('should return object as is with custom id', async () => {
     assert.deepStrictEqual(
       semanticallyCleanObject({ myid: 'a2', a: 'hi' }, 'myid'),
       { myid: 'a2', a: 'hi' }
