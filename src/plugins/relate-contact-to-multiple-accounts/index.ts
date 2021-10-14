8faa73688da4893a87d51e34b112e163ff9907bf
Johannes Verelst <johannes@verelst.net>
2021-10-14 17:15:25 +0200

    Cleaning up

--- src/plugins/relate-contact-to-multiple-accounts/index.ts
+++ src/plugins/relate-contact-to-multiple-accounts/index.ts
@@ -11,6 +11,10 @@ const SELECTORS = {
   CONFIRM_BUTTON: 'input[id$="sharedContactsDisableConfirmButton"]'
 };
 
+type Config = {
+  enabled: boolean;
+};
+
 export class RelateContactToMultipleAccounts extends BrowserforcePlugin {
   public async retrieve(definition?: Config): Promise<Config> {
     const page = await this.browserforce.openPage(PATHS.BASE);
@@ -37,20 +41,8 @@ export class RelateContactToMultipleAccounts extends BrowserforcePlugin {
       },
       config.enabled
     );
-    /* if (config.enabled) { */
-      // When enabling this setting, all we need to do is click the save button
-      await Promise.all([
-        page.click(SELECTORS.SAVE_BUTTON)
-      ]);
-    /*} else {
-      await page.click(SELECTORS.SAVE_BUTTON);
-      console.log('2');
-      await page.waitForSelector(SELECTORS.DISABLE_CONFIRM);
-      console.log('3');
-      await page.click(SELECTORS.DISABLE_CONFIRM);
-      console.log('4');
-      await page.click(SELECTORS.CONFIRM_BUTTON);
-      console.log('5');
-    }*/
+    await Promise.all([
+      page.click(SELECTORS.SAVE_BUTTON)
+    ]);
   }
 }
