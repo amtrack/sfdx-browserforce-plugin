8a6327eab6c470714f7b300f0c1504d6eeade623
Johannes Verelst <johannes@verelst.net>
2021-10-14 17:28:41 +0200

    small cleanups

--- src/plugins/relate-contact-to-multiple-accounts/index.ts
+++ src/plugins/relate-contact-to-multiple-accounts/index.ts
@@ -6,9 +6,7 @@ const PATHS = {
 const SELECTORS = {
   ENABLED: 'input[id$=":sharedContactsCheckBox"]',
   EDIT_BUTTON: 'input[id$=":edit"]',
-  SAVE_BUTTON: 'input[id$=":save"]',
-  DISABLE_CONFIRM: 'input[id$="disable_confirm"]',
-  CONFIRM_BUTTON: 'input[id$="sharedContactsDisableConfirmButton"]'
+  SAVE_BUTTON: 'input[id$=":save"]'
 };
 
 type Config = {
@@ -30,9 +28,11 @@ export class RelateContactToMultipleAccounts extends BrowserforcePlugin {
 
   public async apply(config: Config): Promise<void> {
     const page = await this.browserforce.openPage(PATHS.BASE);
+    // First we have to click the 'Edit' button, to make the checkbox editable
     await page.waitForSelector(SELECTORS.ENABLED);
     await page.click(SELECTORS.EDIT_BUTTON);
     await page.waitForNavigation();
+    // Change the value of the checkbox
     await page.waitForSelector(SELECTORS.ENABLED);
     await page.$eval(
       SELECTORS.ENABLED,
@@ -41,6 +41,7 @@ export class RelateContactToMultipleAccounts extends BrowserforcePlugin {
       },
       config.enabled
     );
+    // Save
     await Promise.all([
       page.click(SELECTORS.SAVE_BUTTON)
     ]);
