6d6bc1871aebbaf4450a352195120364be3e94b3
Clay Chipps <claychipps@gmail.com>
2024-07-19 16:30:33 -0400

    feat: additional work progress

--- src/plugins/service-channel-capacity/index.ts
+++ src/plugins/service-channel-capacity/index.ts
@@ -1,7 +1,7 @@
 import { BrowserforcePlugin } from '../../plugin';
 
 const PATHS = {
-  BASE: 'lightning/setup/ServiceChannelSettings/home'
+  BASE: 'lightning/setup/ServiceChannel'
 };
 
 type Config = {
@@ -9,8 +9,8 @@ type Config = {
   capacityModel: string;
   statusField?: string;
   valuesForInProgress?: string[];
-  checkAgencyCapacityOnReopenedWorkItems?: boolean;
-  checkAgencyCapacityOnReasignedWorkItems?: boolean;
+  checkAgentCapacityOnReopenedWorkItems?: boolean;
+  checkAgentCapacityOnReasignedWorkItems?: boolean;
 };
 
 export class ServiceChannelCapacity extends BrowserforcePlugin {
@@ -19,9 +19,10 @@ export class ServiceChannelCapacity extends BrowserforcePlugin {
 
     // Query for the service channel
     const serviceChannelDeveloperName = definition!.serviceChannelDeveloperName;
-    const serviceChannel = await conn.singleRecordQuery(`SELECT Id FROM ServiceChannel WHERE DeveloperName=${serviceChannelDeveloperName}`);
+    const serviceChannel = await conn.singleRecordQuery(`SELECT Id FROM ServiceChannel WHERE DeveloperName='${serviceChannelDeveloperName}'`);
 
     // Open the service channel setup page
+    console.log(`${PATHS.BASE}/page?address=%2F${serviceChannel.Id}`);
     const page = await this.browserforce.openPage(`${PATHS.BASE}/page?address=%${serviceChannel.Id}`);
 
     // Retrieve the service channel config
@@ -34,10 +35,10 @@ export class ServiceChannelCapacity extends BrowserforcePlugin {
       const valuesForInProgress = await page.$$eval('[id*="capacitySection:statusFieldValuesSection"]', (options) =>
         options.map((option) => option.textContent ?? '')
       );
-      const checkAgencyCapacityOnReopenedWorkItems = await page.$eval('[id*="capacitySection:statusChangeCapacityCheck"]', el => (el.getAttribute('checked') === "checked" ? true : false));
-      const checkAgencyCapacityOnReasignedWorkItems = await page.$eval('[id*="capacitySection:ownerChangeCapacityCheck"]', el => (el.getAttribute('checked') === "checked" ? true : false));
+      const checkAgentCapacityOnReopenedWorkItems = await page.$eval('[id*="capacitySection:statusChangeCapacityCheck"]', el => (el.getAttribute('checked') === "checked" ? true : false));
+      const checkAgentCapacityOnReasignedWorkItems = await page.$eval('[id*="capacitySection:ownerChangeCapacityCheck"]', el => (el.getAttribute('checked') === "checked" ? true : false));
 
-      return { serviceChannelDeveloperName, capacityModel, statusField, valuesForInProgress, checkAgencyCapacityOnReopenedWorkItems, checkAgencyCapacityOnReasignedWorkItems, };
+      return { serviceChannelDeveloperName, capacityModel, statusField, valuesForInProgress, checkAgentCapacityOnReopenedWorkItems, checkAgentCapacityOnReasignedWorkItems, };
     }
 
     return { serviceChannelDeveloperName, capacityModel };
