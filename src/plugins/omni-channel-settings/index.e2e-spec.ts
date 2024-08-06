58072ed90b37e0d9f8e859a1aa0b89cbdff9c748
Clay Chipps <claychipps@gmail.com>
2024-08-06 09:26:30 -0400

    feat: add e2e spec for omni channel settings

---
+++ src/plugins/omni-channel-settings/index.e2e-spec.ts
@@ -0,0 +1,33 @@
+import assert from 'assert';
+import { OmniChannelSettings } from '.';
+
+describe(OmniChannelSettings.name, function () {
+  this.timeout('10m');
+  let plugin: OmniChannelSettings;
+  before(() => {
+    plugin = new OmniChannelSettings(global.bf);
+  });
+
+  const configEnableStatusBasedCapacityModel = {
+    enableStatusBasedCapacityModel: true
+  };
+  const configDisableStatusBasedCapacityModel = {
+    enableStatusBasedCapacityModel: false
+  };
+
+  it('should enable status based capacity model', async () => {
+    await plugin.run(configEnableStatusBasedCapacityModel);
+  });
+  it('should already be set to true', async () => {
+    const res = await plugin.retrieve();
+    assert.deepStrictEqual(res, configEnableStatusBasedCapacityModel);
+  });
+
+  it('should disable status based capacity model', async () => {
+    await plugin.run(configDisableStatusBasedCapacityModel);
+  });
+  it('should already be set to false', async () => {
+    const res = await plugin.retrieve();
+    assert.deepStrictEqual(res, configDisableStatusBasedCapacityModel);
+  });
+});
