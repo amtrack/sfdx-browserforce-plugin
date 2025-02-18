c141fa2151c5d105a7c87ed2c728fb15d41101c0
Clay Chipps <claychipps@gmail.com>
2025-02-17 19:45:22 -0500

    feat: finish initial retrieve impl

--- src/plugins/history-tracking/index.ts
+++ src/plugins/history-tracking/index.ts
@@ -1,12 +1,12 @@
 import { BrowserforcePlugin } from '../../plugin.js';
 
 const PATHS = {
-  BASE: 'lightning/setup/ObjectManager/{APINAME}/FieldsAndRelationships/setHistoryTracking',
+  BASE: 'ui/setup/layout/FieldHistoryTracking?pEntity={APINAME}',
 };
 
 const SELECTORS = {
-  ENABLE_HISTORY: 'input[id="enable"]',
-  ENABLE_FIELD_HISTORY: 'input[id$="{APINAME}_fht"]',
+  ENABLE_HISTORY: 'input[id="enable"][type="checkbox"][name="enable"]',
+  ENABLE_FIELD_HISTORY: 'input[id="{APINAME}_fht"]',
   SAVE_BUTTON: 'input[id$=":save"]',
 };
 
@@ -27,33 +27,12 @@ export class HistoryTracking extends BrowserforcePlugin {
   ): Promise<HistoryTrackingConfig[]> {
     const historyTrackingConfigs: HistoryTrackingConfig[] = [];
 
-    // We need to retrieve the CustomObject details for all custom objects specified in the config
-    // This is because later on, we need the Id when retrieving any CustomField definitions
-    const customObjectsByDeveloperName = new Map();
-    
-    const customObjectApiNames = definition
-      .filter((historyTracking) =>
-        historyTracking.objectApiName.includes('__c')
-      )
-      .map((historyTracking) => `'${historyTracking.objectApiName}'`);
+    // We first need to retrieve the corresponding CustomField.TableOrEnumId for all objects
+    // This is in case we have field tracking configured for custom fields
+    const tableOrEnumIdByObjectApiName =
+      await this.getTableOrEnumIdByObjectApiName(definition);
 
-    if (customObjectApiNames.length > 0) {
-      const customObjectsQuery = await this.org
-        .getConnection()
-        .tooling.query(
-          `SELECT Id, DeveloperName FROM CustomObject WHERE DeveloperName IN (${customObjectApiNames.join(
-            ','
-          )})`
-        );
-
-      for (const customObject of customObjectsQuery.records) {
-        customObjectsByDeveloperName.set(
-          customObject.DeveloperName,
-          customObject.Id
-        );
-      }
-    }
-
+    // Now we can iterate over all history tracking configurations in the definition
     for await (const historyTrackingConfig of definition) {
       const historyTrackingResult = { ...historyTrackingConfig };
 
@@ -63,7 +42,8 @@ export class HistoryTracking extends BrowserforcePlugin {
       );
 
       // Retrieve the object history tracking
-      await page.waitForSelector(`${SELECTORS.ENABLE_HISTORY}`);
+      await page.waitForSelector(SELECTORS.ENABLE_HISTORY);
+
       historyTrackingResult.enableHistoryTracking = await page.$eval(
         SELECTORS.ENABLE_HISTORY,
         (el) => (el.getAttribute('checked') === 'checked' ? true : false)
@@ -75,46 +55,37 @@ export class HistoryTracking extends BrowserforcePlugin {
         continue;
       }
 
-      // We need to retrieve the CustomField details for all custom fields for this config
-      // This is because we need the Id to find the inputs for custom fields in the UI
-      const customFieldsByDeveloperName = new Map();
-
-      const customFieldApiNames = historyTrackingConfig.fieldHistoryTracking
-        .filter((fieldHistoryTracking) =>
-          fieldHistoryTracking.fieldApiName.includes('__c')
-        )
-        .map((fieldHistoryTracking) => fieldHistoryTracking.fieldApiName);
-
-      if (customFieldApiNames) {
-        const customFieldsQuery = await this.org
-          .getConnection()
-          .tooling.query(
-            `SELECT Id, DeveloperName FROM CustomField WHERE DeveloperName IN (${customObjectApiNames.join(
-              ','
-            )}) AND TableEnumOrId = ${
-              customObjectsByDeveloperName.get(
-                historyTrackingConfig.objectApiName
-              ) ?? historyTrackingConfig.objectApiName
-            }`
-          );
-
-        for (const customField of customFieldsQuery.records) {
-          customFieldsByDeveloperName.set(
-            customField.DeveloperName,
-            customField.Id
-          );
-        }
-      }
-
-      // We can now retrieve the field history settings for the fields specified for the object
       const fieldHistoryTrackingConfigs: FieldHistoryTrackingConfig[] = [];
 
+      // If the object history tracking is false, then we already know all field history tracking is false
+      if (!historyTrackingResult.enableHistoryTracking) {
+        for (const fieldHistoryTracking of historyTrackingConfig.fieldHistoryTracking) {
+          fieldHistoryTrackingConfigs.push({
+            ...fieldHistoryTracking,
+            enableHistoryTracking: false,
+          });
+        }
+
+        historyTrackingResult.fieldHistoryTracking = fieldHistoryTrackingConfigs;
+        historyTrackingConfigs.push(historyTrackingResult);
+        continue;
+      }
+
+      // We need to determine the correct html selector for each field that is configured
+      // This is because custom fields are identified using their CustomField.Id value
+      const fieldSelectorByFieldApiName =
+        await this.getFieldSelectorByFieldApiName(
+          tableOrEnumIdByObjectApiName.get(historyTrackingConfig.objectApiName),
+          historyTrackingConfig.fieldHistoryTracking
+        );
+
+      // We can now retrieve the field history settings for the fields specified for the object
       for await (const fieldHistoryTracking of historyTrackingConfig.fieldHistoryTracking) {
         const fieldHistoryTrackingResult = { ...fieldHistoryTracking };
 
-        const fieldApiName =
-          customFieldsByDeveloperName.get(fieldHistoryTracking.fieldApiName) ??
-          fieldHistoryTracking.fieldApiName;
+        const fieldApiName = fieldSelectorByFieldApiName.get(
+          fieldHistoryTracking.fieldApiName
+        );
 
         fieldHistoryTrackingResult.enableHistoryTracking = await page.$eval(
           SELECTORS.ENABLE_FIELD_HISTORY.replace('{APINAME}', fieldApiName),
@@ -132,4 +103,130 @@ export class HistoryTracking extends BrowserforcePlugin {
   }
 
   public async apply(plan: HistoryTrackingConfig[]): Promise<void> {}
+
+  private async getFieldSelectorByFieldApiName(
+    tableEnumOrId: String,
+    fieldHistoryTrackingConfigs: FieldHistoryTrackingConfig[]
+  ) {
+    const fieldSelectorByFieldApiName = new Map();
+
+    const customFieldApiNames = [];
+    const personAccountFieldApiNames = [];
+
+    for (const fieldHistoryTrackingConfig of fieldHistoryTrackingConfigs) {
+      // If this is a person account field, we must do special handling for this
+      if (
+        tableEnumOrId === 'Account' &&
+        fieldHistoryTrackingConfig.fieldApiName.includes('__pc')
+      ) {
+        personAccountFieldApiNames.push(
+          `'${fieldHistoryTrackingConfig.fieldApiName.replace('__pc', '')}'`
+        );
+        console.log(personAccountFieldApiNames);
+        continue;
+      }
+
+      // If this is a custom field, we must query for the Field Id
+      if (fieldHistoryTrackingConfig.fieldApiName.includes('__c')) {
+        customFieldApiNames.push(
+          `'${fieldHistoryTrackingConfig.fieldApiName.replace('__c', '')}'`
+        );
+        continue;
+      }
+
+      // Otherwise, if this is a standard field, the Selector is the Field API Name
+      fieldSelectorByFieldApiName.set(
+        fieldHistoryTrackingConfig.fieldApiName,
+        fieldHistoryTrackingConfig.fieldApiName
+      );
+    }
+
+    if (
+      customFieldApiNames.length === 0 &&
+      personAccountFieldApiNames.length === 0
+    ) {
+      return fieldSelectorByFieldApiName;
+    }
+
+    if (personAccountFieldApiNames.length > 0) {
+      const personAccountFieldsQuery = await this.org
+        .getConnection()
+        .tooling.query(
+          `SELECT Id, DeveloperName FROM CustomField WHERE DeveloperName IN (${personAccountFieldApiNames.join(
+            ','
+          )}) AND TableEnumOrId = 'Contact'`
+        );
+
+      for (const personAccountField of personAccountFieldsQuery.records) {
+        fieldSelectorByFieldApiName.set(
+          `${personAccountField.DeveloperName}__pc`,
+          personAccountField.Id.substring(0, 15)
+        );
+      }
+    }
+
+    if (customFieldApiNames.length > 0) {
+      const customFieldsQuery = await this.org
+        .getConnection()
+        .tooling.query(
+          `SELECT Id, DeveloperName FROM CustomField WHERE DeveloperName IN (${customFieldApiNames.join(
+            ','
+          )}) AND TableEnumOrId = '${tableEnumOrId}'`
+        );
+
+      for (const customField of customFieldsQuery.records) {
+        fieldSelectorByFieldApiName.set(
+          `${customField.DeveloperName}__c`,
+          customField.Id.substring(0, 15)
+        );
+      }
+    }
+
+    return fieldSelectorByFieldApiName;
+  }
+
+  private async getTableOrEnumIdByObjectApiName(
+    historyTrackingConfigs: HistoryTrackingConfig[]
+  ) {
+    const tableOrEnumIdByObjectApiName = new Map();
+
+    const customObjectApiNames = [];
+
+    for (const historyTrackingConfig of historyTrackingConfigs) {
+      // If it is a custom object, the CustomField.TableEnumOrId is the Object Id
+      if (historyTrackingConfig.objectApiName.includes('__c')) {
+        customObjectApiNames.push(
+          `'${historyTrackingConfig.objectApiName.replace('__c', '')}'`
+        );
+        continue;
+      }
+
+      // Otherwise, if this is a standard object, the CustomField.TableEnumOrId is the Object API Name
+      tableOrEnumIdByObjectApiName.set(
+        historyTrackingConfig.objectApiName,
+        historyTrackingConfig.objectApiName
+      );
+    }
+
+    if (customObjectApiNames.length === 0) {
+      return tableOrEnumIdByObjectApiName;
+    }
+
+    const customObjectsQuery = await this.org
+      .getConnection()
+      .tooling.query(
+        `SELECT Id, DeveloperName FROM CustomObject WHERE DeveloperName IN (${customObjectApiNames.join(
+          ','
+        )})`
+      );
+
+    for (const customObject of customObjectsQuery.records) {
+      tableOrEnumIdByObjectApiName.set(
+        `${customObject.DeveloperName}__c`,
+        customObject.Id
+      );
+    }
+
+    return tableOrEnumIdByObjectApiName;
+  }
 }
