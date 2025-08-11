e5382f4fcfabdc0ccd44dcb5f076965a2e55769a
Fleur van Asch <fleurvanasch@casenine.com>
2025-08-11 09:08:17 +0200

    Checkpoint: All the logic done

--- src/plugins/vlocity-features/index.ts
+++ src/plugins/vlocity-features/index.ts
@@ -10,47 +10,60 @@ export class VlocityFeatures extends BrowserforcePlugin {
   private enableFeaturesButton = `::-p-xpath(//a[contains(@title, "Enable Features")])`;
   private configureButton = '::-p-xpath(//button[contains(text(), "Configure")])';
 
-  public async retrieve(): Promise<Config> {
+  public async retrieve(definition): Promise<Config> {
+    const enableStandardCartAPI = definition.enableStandardCartAPI;
     const page = await this.browserforce.openPage(BASE_PATH);
+
     await new Promise(resolve => setTimeout(resolve, 10000));
 
-    let targetFrame;
-    let frames = page.frames();
-    for (const frame of frames) {
-      try {
+    try {
+      let targetFrame;
+      let frames = page.frames();
+      for (const frame of frames) {
         if (frame.url().includes('/apex/')) {
           targetFrame = frame;
           await targetFrame.waitForSelector(this.enableFeaturesButton);
           await targetFrame.click(this.enableFeaturesButton);
         }
-      } catch (e) {
-        console.warn(`'Enable Features' button not found in frame: ${targetFrame.url()}`);
       }
+
+      if (targetFrame) {
+        await new Promise(resolve => setTimeout(resolve, 15000));
+
+        await targetFrame.waitForSelector(this.configureButton);
+        await targetFrame.click(this.configureButton);
+
+        await new Promise(resolve => setTimeout(resolve, 10000));
+
+        let checkboxes = [];
+        if (enableStandardCartAPI) {
+          checkboxes = await targetFrame.$$('::-p-xpath(//input[contains(@type, "checkbox") and contains(@ng-change, "changeInFeature(feature)") and contains(@class, "ng-empty")])');
+        } else {
+          checkboxes = await targetFrame.$$('::-p-xpath(//input[contains(@type, "checkbox") and contains(@ng-change, "changeInFeature(feature)") and contains(@class, "ng-not-empty")])');
+          checkboxes = checkboxes.reverse();
+        }
+
+        for (const checkbox of checkboxes) {
+          await checkbox.click();
+        }
+
+        await new Promise(resolve => setTimeout(resolve, 10000));
+
+        const saveButton = await targetFrame.$('::-p-xpath(//button[contains(text(), "Save")])');
+        if (saveButton) {
+          await saveButton.click();
+          console.log(`Standard Cart API is ${enableStandardCartAPI ? 'enabled' : 'disabled'}`);
+        }
+      }
+    } catch (e) {
+      console.warn(`Something went wrong: ${e}`);
     }
 
-    await new Promise(resolve => setTimeout(resolve, 15000));
-    try {
-      await targetFrame.waitForSelector(this.configureButton);
-      await targetFrame.click(this.configureButton);
-      await new Promise(resolve => setTimeout(resolve, 10000));
-    } catch(e) {
-      console.warn(`Failed to click 'Configure' button`);
-    }
 
-    const checkboxes = await targetFrame.$$('::-p-xpath(//input[contains(@type, "checkbox") and contains(@ng-change, "changeInFeature(feature)") and contains(@class, "ng-empty")])');
-    console.log('checkboxes.length: ' + checkboxes.length);
-
-    for(const checkbox of checkboxes) {
-      await checkbox.click();
-    }
-
-    await new Promise(resolve => setTimeout(resolve, 30000));
-
-
-    return {enableStandardCartAPI: true};
+    return definition;
   }
 
   public async apply(config: Config): Promise<void> {
-    console.log('nu in apply');
+
   }
 }
