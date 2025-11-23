86348df250187400c5271f2ae1d0b7cf41b6675d
Kevin Lindecke <kevin@cloudzen.solutions>
2025-11-23 19:10:29 +0100

    fix test when scratch org has page in name https://page-momentum

--- test/browserforce.e2e-spec.ts
+++ test/browserforce.e2e-spec.ts
@@ -42,9 +42,9 @@ describe('Browserforce', function () {
       );
       const button = await frame.locator('input[name="edit"]');
       assert.notDeepStrictEqual(button, null);
-      assert.ok(!page.url().includes('/page'));
+      assert.ok(!page.url().includes('/page?'));
       await Promise.all([page.waitForNavigation(), button.click()]);
-      assert.ok(page.url().includes('/page'));
+      assert.ok(page.url().includes('/page?'));
       await page.close();
     });
   });
